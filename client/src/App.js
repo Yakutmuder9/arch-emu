4b8326009037139c5c17e7f293bf11b66a88ce56
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-11-22 10:14:09 -0500

    check

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/header/header';
+import Loading from './components/loading/loading';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      
+      <Loading />
     </div>
   )
 }
